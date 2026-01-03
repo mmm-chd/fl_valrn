11b52a2e8f94954581577e0300036b0939957ed1
Severus Melvin <melvinmulyono02@gmail.com>
2026-01-03 15:12:21 +0700

    [SM] icon

--- lib/configs/pages.dart
+++ lib/configs/pages.dart
@@ -2,7 +2,6 @@ import 'package:fl_valrn/bindings/fields_binding.dart';
 import 'package:fl_valrn/bindings/home_binding.dart';
 import 'package:fl_valrn/bindings/login_binding.dart';
 import 'package:fl_valrn/bindings/market_binding.dart';
-import 'package:fl_valrn/bindings/product_binding.dart';
 import 'package:fl_valrn/configs/routes.dart';
 import 'package:fl_valrn/pages/fields_page.dart';
 import 'package:fl_valrn/pages/home_page.dart';
