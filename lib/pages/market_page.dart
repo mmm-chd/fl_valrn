416886ea146a2514173e3be561e7f18e927a7ed0
Severus Melvin <melvinmulyono02@gmail.com>
2026-01-31 17:00:19 +0700

    fix [SM] page

--- lib/pages/market_page.dart
+++ lib/pages/market_page.dart
@@ -19,152 +19,158 @@ class MarketPage extends GetView<MarketController> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.only(left: 16, right: 16, top: 40),
+          padding: const EdgeInsets.only(top: 40),
           child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-
-            /// ===== HEADER LOKASI =====
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Icon(
-                  Icons.pin_drop_sharp,
-                  color: Color(0xff2A9134),
-                  size: 40,
-                ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CustomText(
-                        text: locC.kecamatan.value,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(
+                          Icons.pin_drop_sharp,
+                          color: Color(0xff2A9134),
+                          size: 40,
                         ),
-                      ),
-                      CustomText(
-                        text:
-                            '${locC.kabupaten.value}, ${locC.provinsi.value}',
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
+                        const SizedBox(width: 8),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CustomText(
+                                text: locC.kecamatan.value,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              CustomText(
+                                text:
+                                    '${locC.kabupaten.value}, ${locC.provinsi.value}',
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
+                      ],
+                    ),
+                    CustomSpacing(height: 25),
+
+                    CustomTextfield(
+                      isNumber: false,
+                      controller: controller.searchController,
+                      label: 'Cari tanaman kamu..',
+                    ),
+
+                    CustomSpacing(height: 20),
+
+                    CustomCard(
+                      title: "ngetes",
+                      subtitle: "muncul kaga",
+                      imageUrl:
+                          "https://i.pinimg.com/736x/3a/ee/ee/3aeeee1d04b16f5ab613337aca0721e7.jpg",
+                      isExtendable: false,
+                      isEcommerce: true,
+                      isDescription: false,
+                      width: double.infinity,
+                    ),
+
+                    CustomSpacing(height: 20),
+
+                    Row(
+                      children: const [
+                        CustomText(
+                          text: "Popular Categories",
+                          style: TextStyle(fontWeight: FontWeight.w700),
+                        ),
+                        Spacer(),
+                        CustomText(
+                          text: "See all",
+                          style: TextStyle(color: Color(0xFF2A9134)),
+                        ),
+                      ],
+                    ),
+
+                    CustomSpacing(height: 12),
+                  ],
                 ),
-              ],
-            ),
+              ),
 
-            CustomSpacing(height: 25),
-
-            /// ===== SEARCH =====
-            CustomTextfield(
-              isNumber: false,
-              controller: controller.searchController,
-              label: 'Cari tanaman kamu..',
-            ),
-
-            CustomSpacing(height: 20),
-
-            /// ===== BANNER =====
-            CustomCard(
-              title: "ngetes",
-              subtitle: "muncul kaga",
-              imageUrl:
-                  "https://i.pinimg.com/736x/3a/ee/ee/3aeeee1d04b16f5ab613337aca0721e7.jpg",
-              isExtendable: false,
-              isEcommerce: true,
-              isDescription: false,
-              width: double.infinity,
-            ),
-
-            CustomSpacing(height: 20),
-
-            /// ===== POPULAR =====
-            Row(
-              children: const [
-                CustomText(
-                  text: "Popular Categories",
-                  style: TextStyle(fontWeight: FontWeight.w700),
-                ),
-                Spacer(),
-                CustomText(
-                  text: "See all",
-                  style: TextStyle(color: Color(0xFF2A9134)),
-                ),
-              ],
-            ),
-
-            CustomSpacing(height: 12),
-
-            SizedBox(
-              height: 220,
-              child: Obx(() {
-                if (controller.isLoading.value) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                return ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: controller.trendingCard.length,
-                  itemBuilder: (context, index) {
-                    final item = controller.trendingCard[index];
-                    return CustomCard(
-                      title: item.title,
-                      subtitle: item.subtitle,
-                      imageUrl: item.imageUrl,
-                      height: 152,
-                      width: 120,
-                      isImageLeft: false,
-                    );
-                  },
-                );
-              }),
-            ),
-
-            CustomSpacing(height: 20),
-
-            /// ===== GRID PRODUK =====
-            Obx(() {
-              if (controller.isLoading.value) {
-                return const Center(child: CircularProgressIndicator());
-              }
-
-              return GridView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: controller.productCard.length,
-                gridDelegate:
-                    const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 12,
-                  crossAxisSpacing: 8,
-                  childAspectRatio: 180 / 244,
-                ),
-                itemBuilder: (context, index) {
-                  final item = controller.productCard[index];
-                  return CustomCard(
-                    title: item.title,
-                    subtitle: item.subtitle,
-                    imageUrl: item.imageUrl,
-                    price: item.price.toString(),
-                    isEcommerce: true,
-                    isDescription: true,
-                    onTap: (){
-                      Get.to(
-                        ()=> ProductPage(),
-                        arguments: item,
+              SizedBox(
+                height: 220,
+                child: Obx(() {
+                  if (controller.isLoading.value) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  return ListView.builder(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: controller.trendingCard.length,
+                    itemBuilder: (context, index) {
+                      final item = controller.trendingCard[index];
+                      return CustomCard(
+                        title: item.title,
+                        subtitle: item.subtitle,
+                        imageUrl: item.imageUrl,
+                        height: 152,
+                        width: 120,
+                        isImageLeft: false,
                       );
                     },
                   );
-                },
-              );
-            }),
-          ],
-        )
+                }),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomSpacing(height: 20),
+                    Obx(() {
+                      if (controller.isLoading.value) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+
+                      return GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: controller.productCard.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              mainAxisSpacing: 12,
+                              crossAxisSpacing: 8,
+                              childAspectRatio: 180 / 244,
+                            ),
+                        itemBuilder: (context, index) {
+                          final item = controller.productCard[index];
+                          return CustomCard(
+                            title: item.title,
+                            subtitle: item.subtitle,
+                            imageUrl: item.imageUrl,
+                            price: item.price.toString(),
+                            isEcommerce: true,
+                            isDescription: true,
+                            onTap: () {
+                              Get.to(() => ProductPage(), arguments: item);
+                            },
+                          );
+                        },
+                      );
+                    }),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
